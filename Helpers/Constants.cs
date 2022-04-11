6f3fa151b8ca5c0b6b0d7bd547c4e77cf331590c
Khanyisani Buthelezi <khanyisani.buthelezi03@gmail.com>
2022-04-11 20:24:02 +0200

    Needs versioning

---
+++ Helpers/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KBAlphaBusinessApi.Helpers
+{
+    public class Constants
+    {
+    }
+}
