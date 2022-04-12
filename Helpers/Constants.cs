03c5917de8d6e24584ef592fc23b19ea23fbc63a
Khanyisani Buthelezi <khanyisani.buthelezi03@gmail.com>
2022-04-12 09:04:14 +0200

    added all the constants and api connectors

--- Helpers/Constants.cs
+++ Helpers/Constants.cs
@@ -7,5 +7,47 @@ namespace KBAlphaBusinessApi.Helpers
 {
     public class Constants
     {
+        #region Finaince Constants
+        //Yahoo api key
+        public const string YahooApiKey = "XtEYTQpjGA3rq7OFKwznP5IExLOFpqta5JlwVQx4";
+
+        //Yahoo base url
+        public const string BaseUrl = "https://yfapi.net/";
+
+        //Interactive brockers api key
+        public const string InteractiveBrockersApiKey = "";
+
+        //Interactive brokers
+        public const string InteractiveBrokersBaseUrl = "https://www.interactivebrokers.com/";
+
+        //Polygon api key
+        public const string PolygonApiKey = "_l66fHVP1P0_bCqNR2NXyXHF_T3soUoe";
+
+        //Polygon base url
+        public const string PolygonBaseUrl = "https://api.polygon.io/";
+
+        //OpenAI api key
+        public const string OpenAI_Api_Key = "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";
+
+        //Alpha vantage api key
+        public const string Alpha_Vantage_Api_Key = "BNLRDQ3I58J5U8Y3";
+
+
+        //Alpha vantage base URl
+        public const string AlphaVantageBaseUrl = "https://www.alphavantage.co/";
+
+
+        //Commodaties api Key
+        public const string Commodaties_API_Key = "1ef96isyhzmal4zohvz4vrybfu5ut3k8vsifhpa4y1ip55ml6c9by87l5j11";
+
+        //Commodaties api base url
+        public const string CommodatiesBaseUrl = "https://www.commodities-api.com/api/";
+
+        //Commo Prices api KEy
+        public const string Commo_Prices_API_Key = "xNMy1A4KAyFVf7yoDuxSrn9IMek0hn1W207yobQH7fGoyIy6P830tqH4fuWf";
+
+        //Commo Prices api base url
+        public const string CommoPricesBaseUrl = "https://api.commoprices.com/";
+        #endregion
     }
 }
---
+++ Services/APIConnector.cs
@@ -0,0 +1,146 @@
+﻿using KBAlphaBusinessApi.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace KBAlphaBusinessApi.Services
+{
+    public class APIConnector
+    {
+        #region Finaince connectors
+        //Alpha vantage api connector
+        public static HttpResponseMessage Start_Alpha_Vantage_Connection(string endpoint)
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    client.DefaultRequestHeaders.Clear();
+
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("apikey", "demo");
+
+                    var data = client.GetAsync(endpoint).GetAwaiter().GetResult();
+
+                    return data;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        //Commodaties api connector
+        public static HttpResponseMessage Start_Commadaties_Connection(string endpoint)
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    client.DefaultRequestHeaders.Clear();
+
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    var data = client.GetAsync(endpoint).GetAwaiter().GetResult();
+
+                    return data;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
+        //Polyogon api connector
+        public static HttpResponseMessage Start_Polygon_API_Connection(string endpoint)
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    client.DefaultRequestHeaders.Accept.Clear();
+
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", Constants.PolygonApiKey);
+
+                    client.BaseAddress = new Uri(endpoint);
+
+                    var response = client.GetAsync(endpoint).GetAwaiter().GetResult();
+
+                    return response;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        //Yahoo api connector 
+        public static HttpResponseMessage Start_Yahoo_API_Conection(string endpoint)
+        {
+
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    client.DefaultRequestHeaders.Accept.Clear();
+
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    //client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("x-api-key", Constants.YahooApiKey);
+                    client.DefaultRequestHeaders.Add("x-api-key", Constants.YahooApiKey);
+
+
+                    client.BaseAddress = new Uri(endpoint);
+
+                    var response = client.GetAsync(endpoint).GetAwaiter().GetResult();
+
+                    return response;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+
+        //Commo Prices api connector
+        public static HttpResponseMessage Start_Commo_Prices_Connection(string url)
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    client.DefaultRequestHeaders.Accept.Clear();
+
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", Constants.Commo_Prices_API_Key);
+
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+
+                    var data = client.GetAsync(url).GetAwaiter().GetResult();
+
+                    return data;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        #endregion
+    }
+}
